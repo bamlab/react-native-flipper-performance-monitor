5998d7dc5bfc247ed982a1aa62cb6cadbe2fdc26
almouro <contact@almouro.com>
2022-01-21 14:51:09 +0100

    ref(desktop): extract PerfMonitorView

---
+++ flipper-desktop/src/PerfMonitorView.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { Chart } from "./components/Chart";
+import { Report } from "./Report";
+import { Controls } from "./components/Controls";
+import { ScrollContainer } from "./components/ScrollContainer";
+import { Title } from "./components/Title";
+import { Measure } from "./types/Measure";
+
+const sanitizeData = (fps: number) => {
+  if (fps > 60) return 60;
+  if (fps < 0) return 0;
+  return Math.ceil(fps);
+};
+
+// This is the same value as defined here: https://github.com/bamlab/react-native-performance/blob/master/flipper-android/src/main/java/tech/bam/rnperformance/FPSMonitor.java#L42
+const MEASURE_INTERVAL = 500;
+const formatFpsToXY = (fps: number[]): { x: number; y: number }[] => {
+  return fps.map((y, index) => ({
+    x: index * MEASURE_INTERVAL,
+    y,
+  }));
+};
+
+export const PerfMonitorView = ({
+  measures,
+  startMeasuring,
+  stopMeasuring,
+  isMeasuring,
+}: {
+  measures: Measure[];
+  startMeasuring: () => void;
+  stopMeasuring: () => void;
+  isMeasuring: boolean;
+}) => {
+  const getFPSGraphData = (key: "JS" | "UI") => {
+    return formatFpsToXY(
+      measures
+        .map((measure) => (measure[key] / measure.expected) * 60)
+        .map(sanitizeData)
+    );
+  };
+
+  return (
+    <ScrollContainer scrollable>
+      <Title />
+      {!isMeasuring && measures.length > 0 ? (
+        <Report measures={measures} />
+      ) : null}
+      <Controls
+        isMeasuring={isMeasuring}
+        start={startMeasuring}
+        stop={stopMeasuring}
+      />
+      <Chart data={getFPSGraphData("JS")} height={350} title="JS FPS" />
+      <Chart data={getFPSGraphData("UI")} height={350} title="UI FPS" />
+    </ScrollContainer>
+  );
+};
