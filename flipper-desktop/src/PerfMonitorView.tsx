4a07b0f3e5ca55ebad64ac50e20c9fb8385c15a7
almouro <contact@almouro.com>
2022-01-24 15:06:40 +0100

    ref(desktop): extract time controls component

--- flipper-desktop/src/PerfMonitorView.tsx
+++ flipper-desktop/src/PerfMonitorView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { ReactNode, useEffect, useState } from "react";
 import { Chart } from "./components/Chart";
 import { Report } from "./Report";
 import { StartButton } from "./components/StartButton";
@@ -18,6 +18,60 @@ const sanitizeData = (fps: number) => {
   if (fps < 0) return 0;
   return Math.ceil(fps);
 };
+
+const ControlsContainer = ({ children }: { children: ReactNode }) => (
+  <div
+    style={{
+      margin: 10,
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const TimeLimitControl = ({
+  timeLimitEnabled,
+  toggleTimeLimit,
+  setTimeLimit,
+  timeLimit,
+}: {
+  timeLimitEnabled: boolean;
+  timeLimit: number | null;
+  setTimeLimit: (limit: number | null) => void;
+  toggleTimeLimit: (checked: boolean) => void;
+}) => (
+  <div
+    style={{
+      marginLeft: 10,
+      display: "flex",
+      flexDirection: "row",
+      alignItems: "center",
+    }}
+  >
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={timeLimitEnabled}
+          onChange={(event: { target: { checked: boolean } }) => {
+            toggleTimeLimit(event.target.checked);
+          }}
+        />
+      }
+      label="Enable time limit of "
+    />
+    <TextField
+      type="number"
+      onChange={({ target: { value } }) =>
+        setTimeLimit(Math.floor(parseInt(value, 10)))
+      }
+      defaultValue={timeLimit}
+    />
+    <Typography>ms</Typography>
+  </div>
+);
 
 // This is the same value as defined here: https://github.com/bamlab/react-native-performance/blob/master/flipper-android/src/main/java/tech/bam/rnperformance/FPSMonitor.java#L42
 const MEASURE_INTERVAL = 500;
@@ -41,9 +95,6 @@ export const PerfMonitorView = ({
   const DEFAULT_TIME_LIMIT = 10000;
   const [timeLimitEnabled, setTimeLimitEnabled] = useState(true);
   const [timeLimit, setTimeLimit] = useState<number | null>(DEFAULT_TIME_LIMIT);
-  const toggleTimeLimit = (event: { target: { checked: boolean } }) => {
-    setTimeLimitEnabled(event.target.checked);
-  };
   useEffect(() => {
     if (timeLimit && measures.length > timeLimit / MEASURE_INTERVAL) {
       stopMeasuring();
@@ -56,43 +107,19 @@ export const PerfMonitorView = ({
     <ScrollContainer>
       <Title />
       <Report measures={measures} isMeasuring={isMeasuring} />
-      <div
-        style={{
-          margin: 10,
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
+      <ControlsContainer>
         <StartButton
           isMeasuring={isMeasuring}
           start={startMeasuring}
           stop={stopMeasuring}
         />
-        <div
-          style={{
-            marginLeft: 10,
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
-          }}
-        >
-          <FormControlLabel
-            control={
-              <Checkbox checked={timeLimitEnabled} onChange={toggleTimeLimit} />
-            }
-            label="Enable time limit of "
-          />
-          <TextField
-            type="number"
-            onChange={({ target: { value } }) =>
-              setTimeLimit(Math.floor(parseInt(value, 10)))
-            }
-            defaultValue={timeLimit}
-          />
-          <Typography>ms</Typography>
-        </div>
-      </div>
+        <TimeLimitControl
+          timeLimitEnabled={timeLimitEnabled}
+          toggleTimeLimit={setTimeLimitEnabled}
+          setTimeLimit={setTimeLimit}
+          timeLimit={timeLimit}
+        />
+      </ControlsContainer>
       <Chart
         data={getFPSGraphData("JS")}
         height={350}
