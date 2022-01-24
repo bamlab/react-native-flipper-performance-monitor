638f3bde7a73dadec6f4cc2da3fc407112563128
almouro <contact@almouro.com>
2022-01-24 11:37:21 +0100

    feat(desktop): add checkbox to enable time limit

--- flipper-desktop/src/PerfMonitorView.tsx
+++ flipper-desktop/src/PerfMonitorView.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Chart } from "./components/Chart";
 import { Report } from "./Report";
 import { Controls } from "./components/Controls";
 import { ScrollContainer } from "./components/ScrollContainer";
 import { Title } from "./components/Title";
 import { Measure } from "./types/Measure";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
 
 const sanitizeData = (fps: number) => {
   if (fps > 60) return 60;
@@ -31,12 +32,23 @@ export const PerfMonitorView = ({
       .map((measure) => (measure[key] / measure.expected) * 60)
       .map(sanitizeData);
 
+  const [timeLimit, setTimeLimit] = useState<number | null>(10000);
+  const handleChange = (event: { target: { checked: boolean } }) => {
+    setTimeLimit(event.target.checked ? 10000 : null);
+  };
+
   return (
     <ScrollContainer scrollable>
       <Title />
       {!isMeasuring && measures.length > 0 ? (
         <Report measures={measures} />
       ) : null}
+
+      <FormControlLabel
+        control={<Checkbox checked={!!timeLimit} onChange={handleChange} />}
+        label="Set time limit"
+      />
+
       <Controls
         isMeasuring={isMeasuring}
         start={startMeasuring}
@@ -47,12 +59,14 @@ export const PerfMonitorView = ({
         height={350}
         title="JS FPS"
         interval={MEASURE_INTERVAL}
+        timeLimit={timeLimit}
       />
       <Chart
         data={getFPSGraphData("UI")}
         height={350}
         title="UI FPS"
         interval={MEASURE_INTERVAL}
+        timeLimit={timeLimit}
       />
     </ScrollContainer>
   );
