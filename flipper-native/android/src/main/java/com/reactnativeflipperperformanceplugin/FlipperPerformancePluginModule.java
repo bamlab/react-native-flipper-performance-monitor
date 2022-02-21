f994c2290ccd88ba023eac8c990892a3e03a3e0f
almouro <contact@almouro.com>
2022-02-21 10:15:46 +0100

    feat(native): generate modules with builder-bob

---
+++ flipper-native/android/src/main/java/com/reactnativeflipperperformanceplugin/FlipperPerformancePluginModule.java
@@ -0,0 +1,34 @@
+package com.reactnativeflipperperformanceplugin;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = FlipperPerformancePluginModule.NAME)
+public class FlipperPerformancePluginModule extends ReactContextBaseJavaModule {
+    public static final String NAME = "FlipperPerformancePlugin";
+
+    public FlipperPerformancePluginModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
+
+    @Override
+    @NonNull
+    public String getName() {
+        return NAME;
+    }
+
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    public void multiply(int a, int b, Promise promise) {
+        promise.resolve(a * b);
+    }
+
+    public static native int nativeMultiply(int a, int b);
+}
