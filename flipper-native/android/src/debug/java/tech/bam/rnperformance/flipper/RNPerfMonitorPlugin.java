647489aec9b1c50693c7d7b28d18a7767b3c8ac5
Alexandre Moureaux <Almouro@users.noreply.github.com>
2022-06-20 11:42:30 +0200

    feat(native): add expo compatibility for android (#56)
    * feat(native): add expo compatibility for android
    
    * prune(native): remove deprecated annotation

--- flipper-native/android/src/debug/java/tech/bam/rnperformance/flipper/RNPerfMonitorPlugin.java
+++ flipper-native/android/src/debug/java/tech/bam/rnperformance/flipper/RNPerfMonitorPlugin.java
@@ -4,23 +4,45 @@ import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
+
+import com.facebook.flipper.android.AndroidFlipperClient;
 import com.facebook.flipper.core.FlipperConnection;
 import com.facebook.flipper.core.FlipperObject;
 import com.facebook.flipper.core.FlipperPlugin;
 import com.facebook.flipper.core.FlipperReceiver;
 import com.facebook.flipper.core.FlipperResponder;
 import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.bridge.ReactApplicationContext;
 
 public class RNPerfMonitorPlugin implements FlipperPlugin {
     private FlipperConnection connection;
-    private ReactInstanceManager reactInstanceManager;
     private FPSMonitor fpsMonitor;
+    private ReactApplicationContext reactContext;
 
+    public static void passReactApplicationContext(@NonNull ReactApplicationContext reactContext) {
+		final RNPerfMonitorPlugin plugin = AndroidFlipperClient.getInstance(reactContext.getBaseContext())
+				.getPluginByClass(RNPerfMonitorPlugin.class);
+
+		if (plugin != null) {
+			plugin.setReactContext(reactContext);
+		} else {
+			Log.d(RNPerfMonitorPlugin.class.getName(), "Flipper plugin isn't installed");
+		}
+	}
+
+	// Keeping this constructor for retrocompatibility
     public RNPerfMonitorPlugin(ReactInstanceManager reactInstanceManager) {
-        this.reactInstanceManager = reactInstanceManager;
         fpsMonitor = new FPSMonitor();
     }
 
+	public RNPerfMonitorPlugin() {
+		fpsMonitor = new FPSMonitor();
+	}
+
+    public void setReactContext(@NonNull ReactApplicationContext reactContext) {
+        this.reactContext = reactContext;
+	}
 
     @Override
     public String getId() {
@@ -61,7 +83,7 @@ public class RNPerfMonitorPlugin implements FlipperPlugin {
         new Handler(Looper.getMainLooper()).post(new Runnable() {
             @Override
             public void run() {
-                fpsMonitor.start(reactInstanceManager.getCurrentReactContext(), new MonitorCallback() {
+                fpsMonitor.start(reactContext, new MonitorCallback() {
                     @Override
                     public void setCurrentFPS(int uiFrames, int jsFrames, int timeInMs) {
                         if (connection == null) {
