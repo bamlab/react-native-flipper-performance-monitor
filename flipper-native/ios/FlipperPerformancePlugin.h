d16c88ed95ef7f96c2aafbd4c42bda58fe9d192d
almouro <contact@almouro.com>
2022-07-01 11:04:34 +0200

    fix(native): ensure we don't require flipper on iOS if not installed

--- flipper-native/ios/FlipperPerformancePlugin.h
+++ flipper-native/ios/FlipperPerformancePlugin.h
@@ -1,8 +1,15 @@
 #import <Foundation/Foundation.h>
 
+#if __has_include(<FlipperKit/FlipperPlugin.h>)
 #import <FlipperKit/FlipperPlugin.h>
 #import <React/RCTBridge.h>
 
 @interface FlipperPerformancePlugin : NSObject<RCTBridgeModule, FlipperPlugin>
 
 @end
+
+#else
+@interface FlipperPerformancePlugin : NSObject
+
+@end
+#endif
