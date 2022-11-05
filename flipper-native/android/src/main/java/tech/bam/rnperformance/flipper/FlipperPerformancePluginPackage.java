406f5337b660f30ed22d523c2a86b50f360ca57f
almouro <contact@almouro.com>
2022-11-05 11:32:55 +0100

    Revert "feat(native): add expo compatibility for android (#56)"
    This reverts commit 647489aec9b1c50693c7d7b28d18a7767b3c8ac5.

--- flipper-native/android/src/main/java/tech/bam/rnperformance/flipper/FlipperPerformancePluginPackage.java
+++ flipper-native/android/src/main/java/tech/bam/rnperformance/flipper/FlipperPerformancePluginPackage.java
@@ -7,7 +7,6 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -18,11 +17,6 @@ public class FlipperPerformancePluginPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(@NonNull ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new FlipperPerformancePluginModule(reactContext));
-
-		if (BuildConfig.DEBUG) {
-			passReactContextToFlipperPlugin(reactContext);
-		}
-
         return modules;
     }
 
@@ -31,20 +25,4 @@ public class FlipperPerformancePluginPackage implements ReactPackage {
     public List<ViewManager> createViewManagers(@NonNull ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
-
-    private void passReactContextToFlipperPlugin(@NonNull ReactApplicationContext reactContext) {
-		try {
-			/*
-			 We use reflection since Flipper library is not available in release mode
-			 We pass React context here, for compatibility with expo apps
-			 See https://github.com/expo/expo/issues/17852
-			*/
-			Class<?> aClass = Class.forName("tech.bam.rnperformance.flipper.RNPerfMonitorPlugin");
-			aClass
-					.getMethod("passReactApplicationContext", ReactApplicationContext.class)
-					.invoke(null, reactContext);
-		} catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
-			e.printStackTrace();
-		}
-	}
 }
